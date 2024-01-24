c7cc0ca636f34450ef0be731074cb5bdd71026b8
abhishek-8081 <abhishekrajputji2004@gmail.com>
2024-01-24 15:03:52 +0530

    add changes

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/city.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Picture of Beautiful City";
 
 /**
  * Project list
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "My Blog Website 🎉",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "This is my Blog website where I write about my experiences and learnings in the tech industry ",
+    url: " https://docs.google.com/presentation/d/1C50XPcImpYUsF1E2HqJhsIjgQHO7DYRJ/edit?usp=sharing&ouid=108577071219008507393&rtpof=true&sd=true",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Calculator Website",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "This is the Calculator website from where you can calculate the mathematical Problems ",
+    url: " https://docs.google.com/presentation/d/1C50XPcImpYUsF1E2HqJhsIjgQHO7DYRJ/edit?usp=sharing&ouid=108577071219008507393&rtpof=true&sd=true",
   },
   {
-    title: "My Resume Site",
+    title: "My Linkedin Profile ",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+      "You can Visit to my linkedin profile by Clicking here  ",
+    url: "https://www.linkedin.com/in/abhishek-kumar-170492258/",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "Twitter Profile ",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "You can visit my Twitter profile by Clicking here . ",
+    url: "https://www.linkedin.com/in/abhishek-kumar-170492258/",
   },
 ];
 
